ca800218d2d9c46e1eb5ef745a154e2a998a7d5e
LGM <luisgilmartin@gmail.com>
2026-04-29 17:30:56 +0200

    Last (hopefully)

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -6,6 +6,9 @@ INPUTS=("sample_2loop.txt" "abelian_higgs_mass.txt" "abelian_fermion_3loop.txt")
 OUTPUT="$BASE_DIR/outputs"
 TIME_FILE="$BASE_DIR/times.txt"
 
+PARAMS_FILE1="params_sample.txt"
+PARAMS_FILE2="params_abelian.txt"
+
 echo "File | Time (seconds)" > $TIME_FILE
 echo "--------------------------" >> $TIME_FILE
 
@@ -13,9 +16,15 @@ for filename in "${INPUTS[@]}"; do
     input_path="$BASE_DIR/inputs/$filename"
     output_path="$OUTPUT/$filename"
 
-    echo -e "\n\n\nProcessing $filename... \n\n\n"
+    if [[ "$filename" == "sample_2loop.txt" ]]; then
+        PARAMS="$PARAMS_FILE1"
+    else
+        PARAMS="$PARAMS_FILE2"
+    fi
 
-    elapsed=$( (time -p sirena "$input_path" "$output_path" -p "params.txt" -vv) 2>&1 \
+    echo -e "\n\n\nProcessing $filename with $PARAMS... \n\n\n"
+
+    elapsed=$( (time -p sirena "$input_path" "$output_path" -p "$PARAMS" -vv) 2>&1 \
             | tee /dev/stderr \
             | awk '/real/ {print $2}' )
 
